e040fb6b1e96aa1669bf8b91d57b2d88a0825690
Ivan Draskovic <ivnddev@gmail.com>
2022-09-12 13:09:10 +0200

    About Description ... grid not fully responsive

---
+++ src/components/AboutDesctiption.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function AboutDesctipition() {
+  return (
+    <div className="container">
+      <section className=" content main-info-area ">
+        <div className="img-container">
+          <img
+            src={require("../images/sposobnizgodni.png")}
+            alt="ZgodniPametni"
+          />
+        </div>
+        <div className="main-description ">
+          <p>
+            Aut saepe reiciendis et ipsam inventore eum voluptatem rerum eum
+            illum molestiae et molestiae error id provident esse ut odio
+            laboriosam. Ad quod molestiae aut blanditiis eius non soluta neque
+            ad itaque aut eaque recusandae sit dicta voluptas non neque
+            officiis.Ad quod molestiae aut blanditiis eius non soluta neque ad
+            itaque aut eaque{" "}
+          </p>
+          <p>
+            Ab dolor dignissimos At laborum natus ab asperiores possimus aut
+            ducimus tenetur non suscipit quas eos voluptas aperiam et neque
+            enim. Ratione laborum sed voluptatum facere et repellendus sunt aut
+            aspernatur minima aut aspernatur recusandae ad eveniet harum qui
+            amet odit. Est alias dolores aut quia explicabo ut error vero id
+            quae veniam aut pariatur dicta.Ratione laborum sed voluptatum facere
+            et repellendus sunt aut aspernatur minima aut aspernatur recusandae
+            ad eveniet harum qui amet odit{" "}
+          </p>
+        </div>
+        <div className="mission-statement">
+          <h3>MISSION STATEMENT</h3>
+          <p>
+            <p>
+              A ducimus quam qui molestias quasi ut mollitia minus est odio
+              voluptatibus in corporis quisquam quo dolores alias ut esse
+              laudantium. Ea omnis blanditiis sed itaque fuga id quam animi
+              dicta labore ut dolor unde. Ea dolorum omnis sed voluptatum magni
+              ea eius nihil At fugiat numquam. Qui quia suscipit a nisi natus
+              aut labore similique quo adipisci dolor.{" "}
+            </p>
+          </p>
+        </div>
+        <div>
+          <h3>FUN FACTS </h3>
+          <p>
+            Et quam deleniti aut minima error sed beatae maxime et labore nemo
+            in deleniti dolore. Eos laborum incidunt et nostrum facilis rem
+            deserunt omnis? Vel quidem perspiciatis est voluptatem expedita aut
+            veritatis recusandae eum nihil natus qui enim labore. Et voluptatum
+            ut suscipit autem aut obcaecati assumenda est sequi unde.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,12 @@
 import React from "react";
+import AboutDesctipition from "../components/AboutDesctiption";
 import MainTitle from "../components/MainTitle";
 
 export default function About() {
   return (
     <main>
       <MainTitle title={"ABOUT MY BUSINESS"} />
-      <h1>About Page Dralje</h1>
+      <AboutDesctipition />
     </main>
   );
 }
