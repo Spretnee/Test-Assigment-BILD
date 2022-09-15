937ad819bcc936e7d6aef6c14f6d9f8159d78db3
Ivan Draskovic <ivnddev@gmail.com>
2022-09-15 13:50:11 +0200

    introduction video done without popup

--- src/pages/home/homeComponents/introductionVideo/IntroductionVideo.js
+++ src/pages/home/homeComponents/introductionVideo/IntroductionVideo.js
@@ -3,8 +3,25 @@ import "./introductionVideo.css";
 
 const IntroductionVideo = () => {
   return (
-    <section className="introduction-video">
-      <h1>Video section here</h1>
+    <section className="introduction-video ">
+      <div className="video-section container">
+        <div className="video-wrapper"></div>
+        <h2>get to know us a little better</h2>
+        <p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad neque
+            minus suscipit repudiandae totam fugit assumenda enim nisi id
+            consequuntur quod est sit, ut ratione necessitatibus deserunt.
+            Inventore quia odit, exercitationem aliquam consequatur et
+            perferendis similique eius saepe ipsa. Dolor accusamus est deleniti
+            tempore nulla placeat ullam. Rem deleniti cumque sequi? Consectetur
+            numquam eius consequuntur quaerat cum ex provident reiciendis
+            reprehenderit corporis necessitatibus atque delectus, laudantium
+            expedita perferendis voluptate ullam cumque inventore eligendi
+            pariatur corrupti, beatae sequi harum!
+          </p>
+        </p>
+      </div>
     </section>
   );
 };
